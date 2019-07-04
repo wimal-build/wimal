bf96f5c36c22f8d67efbd3f5c90ca4faf1821b01
yang.yu <yang.yu@disigma.org>
2019-07-04 22:36:10 +0800

    [ld64] Use blocks runtime from compiler-rt.
    - [libobjc2] Remove libobjc2.

--- ld64/port/Block.h
+++ ld64/port/Block.h
@@ -1,5 +1,7 @@
 #ifdef __APPLE__
 #include_next <Block.h>
 #else // !__APPLE__
-#include <objc/blocks_runtime.h>
+// Use blocks runtime from compiler-rt.
+// http://releases.llvm.org/8.0.0/compiler-rt-8.0.0.src.tar.xz
+#include "BlocksRuntime/Block.h"
 #endif // __APPLE__
---
+++ ld64/port/runtime.c
@@ -0,0 +1,9 @@
+#ifndef __APPLE__
+
+// BlocksRuntime
+#define HAVE_SYNC_BOOL_COMPARE_AND_SWAP_INT
+#define HAVE_SYNC_BOOL_COMPARE_AND_SWAP_LONG
+#include "BlocksRuntime/data.c"
+#include "BlocksRuntime/runtime.c"
+
+#endif // __APPLE__

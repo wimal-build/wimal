e984d8c091a460d28b119cae6a35cfde78103ddf
disigma <yang.yu@disigma.org>
2024-05-27 21:46:07 +0800

    [cctools] Build apple-otool and apple-libtool

---
+++ cctools/port/mach/sparc/thread_status.h
@@ -0,0 +1,2 @@
+#include <llvm-c/Disassembler.h>
+#include_next <mach/sparc/thread_status.h>
--- cctools/port/objc/objc-runtime.h
+++ cctools/port/objc/objc-runtime.h
@@ -1,8 +1,16 @@
 #ifdef __APPLE__
 #include_next <objc/objc-runtime.h>
-#else // !__APPLE__
-// https://opensource.apple.com/source/objc4/objc4-750.1/runtime/runtime.h
-#define CLS_GETINFO(cls,infomask) ((cls)->info & (infomask))
-#define CLS_CLASS 0x1
-#define CLS_META 0x2
 #endif // __APPLE__
+
+// https://opensource.apple.com/source/objc4/objc4-750.1/runtime/runtime.h
+#ifndef CLS_GETINFO
+#define CLS_GETINFO(cls,infomask) ((cls)->info & (infomask))
+#endif // CLS_GETINFO
+
+#ifndef CLS_CLASS
+#define CLS_CLASS 0x1
+#endif // CLS_CLASS
+
+#ifndef CLS_META
+#define CLS_META 0x2
+#endif // CLS_META
